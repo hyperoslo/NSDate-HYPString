2701adf2c5728be7474993b3be9abdf1290676b9
Elvis Nuñez <elvisnunez@me.com>
2015-03-16 11:14:34 +0100

    Move convenience methods to a pod

---
+++ Tests/Pods/NSDate-HYPConvenience/README.md
@@ -0,0 +1,31 @@
+# NSDate-HYPConvenience
+
+[![CI Status](http://img.shields.io/travis/hyperoslo/NSDate-HYPConvenience.svg?style=flat)](https://travis-ci.org/hyperoslo/NSDate-HYPConvenience)
+[![Version](https://img.shields.io/cocoapods/v/NSDate-HYPConvenience.svg?style=flat)](http://cocoadocs.org/docsets/NSDate-HYPConvenience)
+[![License](https://img.shields.io/cocoapods/l/NSDate-HYPConvenience.svg?style=flat)](http://cocoadocs.org/docsets/NSDate-HYPConvenience)
+[![Platform](https://img.shields.io/cocoapods/p/NSDate-HYPConvenience.svg?style=flat)](http://cocoadocs.org/docsets/NSDate-HYPConvenience)
+
+## Usage
+
+```objc
++ (NSDate *)hyp_dateForDay:(NSInteger)day month:(NSInteger)month year:(NSInteger)year;
+
++ (NSDate *)hyp_timeForHour:(NSInteger)hour minute:(NSInteger)minute;
+```
+
+## Installation
+
+**NSDate-HYPConvenience** is available through [CocoaPods](http://cocoapods.org). To install
+it, simply add the following line to your Podfile:
+
+```ruby
+pod 'NSDate-HYPConvenience'
+```
+
+## Author
+
+Hyper, teknologi@hyper.no
+
+## License
+
+**NSDate-HYPConvenience** is available under the MIT license. See the LICENSE file for more info.
--- Tests/Tests/Tests.m
+++ Tests/Tests/Tests.m
@@ -2,6 +2,8 @@
 
 #import "NSDate+HYPString.h"
 
+#import "NSDate+HYPConvenience.h"
+
 @interface Tests : XCTestCase
 
 @end
@@ -10,40 +12,40 @@
 
 - (void)testTimeString
 {
-    NSDate *date = [self timeForHour:10 minute:10];
+    NSDate *date = [NSDate hyp_timeForHour:10 minute:10];
     XCTAssertEqualObjects([date hyp_timeString], @"10:10");
 }
 
 - (void)testDateString
 {
-    NSDate *date = [self dateForDay:1 month:1 year:2010];
+    NSDate *date = [NSDate hyp_dateForDay:1 month:1 year:2010];
     XCTAssertEqualObjects([date hyp_dateString], @"2010-01-01");
 }
 
 - (void)testDateStringWithFormat
 {
-    NSDate *date = [self dateForDay:1 month:1 year:2010];
+    NSDate *date = [NSDate hyp_dateForDay:1 month:1 year:2010];
     XCTAssertEqualObjects([date hyp_dateStringWithFormat:@"dd.MM.yyyy"], @"01.01.2010");
 }
 
 - (void)testTimeRangeStringToEndDate
 {
-    NSDate *startDate = [self timeForHour:10 minute:10];
-    NSDate *endDate = [self timeForHour:12 minute:12];
+    NSDate *startDate = [NSDate hyp_timeForHour:10 minute:10];
+    NSDate *endDate = [NSDate hyp_timeForHour:12 minute:12];
     XCTAssertEqualObjects([startDate hyp_timeRangeStringToEndDate:endDate], @"10:10 - 12:12");
 }
 
 - (void)testDateRangeStringToEndDate
 {
-    NSDate *startDate = [self dateForDay:1 month:1 year:2010];
-    NSDate *endDate = [self dateForDay:2 month:2 year:2012];
+    NSDate *startDate = [NSDate hyp_dateForDay:1 month:1 year:2010];
+    NSDate *endDate = [NSDate hyp_dateForDay:2 month:2 year:2012];
     XCTAssertEqualObjects([startDate hyp_dateRangeStringToEndDate:endDate], @"2010-01-01 - 2012-02-02");
 }
 
 - (void)testDateRangeStringToEndDateWithFormat
 {
-    NSDate *startDate = [self dateForDay:1 month:1 year:2010];
-    NSDate *endDate = [self dateForDay:2 month:2 year:2012];
+    NSDate *startDate = [NSDate hyp_dateForDay:1 month:1 year:2010];
+    NSDate *endDate = [NSDate hyp_dateForDay:2 month:2 year:2012];
     XCTAssertEqualObjects([startDate hyp_dateRangeStringToEndDate:endDate withFormat:@"dd.MM.yyyy"], @"01.01.2010 - 02.02.2012");
 }
 
